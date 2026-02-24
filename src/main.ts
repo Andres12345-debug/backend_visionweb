d9c57cc5c0bf7fbe13190bf32b7fd6532c7fe4e6
Andres12345-debug <julian2000mk1@gmail.com>
2026-02-23 20:57:08 -0500

    Cambio main.ts para puerto servidor

--- src/main.ts
+++ src/main.ts
@@ -3,27 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const puerto =
-    Number(process.env.PORT) ||
-    Number(process.env.PUERTO_SERVIDOR) ||
-    3000;
+  const puerto = Number(process.env.PORT) || 8080;
 
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
-  // 🔥 Activar validaciones globales
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,              // elimina campos no definidos en el DTO
-      forbidNonWhitelisted: true,   // lanza error si envían campos extra
-      transform: true,              // transforma tipos automáticamente
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
-  await app.listen(puerto, () => {
-    console.log(`🚀 Servidor funcionando en puerto: ${puerto}`);
-  });
+  await app.listen(puerto);
+  console.log(`🚀 Servidor funcionando en puerto: ${puerto}`);
 }
 
 bootstrap();
